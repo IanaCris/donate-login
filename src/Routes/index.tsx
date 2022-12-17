594a5ff20279f078476d83321b4b2a58c67c75f3
IanaCris <ianasousati@gmail.com>
2022-12-17 18:22:18 -0300

    Initial commit

---
+++ src/Routes/index.tsx
@@ -0,0 +1,29 @@
+import { StackScreenProps } from "@react-navigation/stack";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+import LoginPage from "../pages/Login";
+import ListagemPage from "../pages/Listagem";
+import CadastroPage from "../pages/Cadastro";
+import { Usuario } from "../model/usuario";
+
+export type ScreenNavigationProp = StackScreenProps<{
+    Login: undefined,
+    Listagem: { usuario: Usuario },
+    Cadastro: {}
+}>
+
+const Stack = createNativeStackNavigator();
+
+export default function () {
+
+    return (
+        <NavigationContainer>
+            <Stack.Navigator>
+                <Stack.Screen name="Login" component={LoginPage} options={{ headerTitle: "Login" }} />
+                <Stack.Screen name="Listagem" component={ListagemPage} options={{ headerTitle: "Lista de Usuários" }} />
+                <Stack.Screen name="Cadastro" component={CadastroPage} />
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
+}
---
+++ src/pages/Login/index.tsx
@@ -0,0 +1,92 @@
+import { useState, useEffect } from 'react';
+import { useNavigation } from "@react-navigation/native"
+import { Alert, Image, Text, TextInput, TouchableOpacity, View } from 'react-native';
+
+import { ScreenNavigationProp } from '../../Routes';
+import { Usuario } from '../../model/usuario';
+import styles from './styles';
+
+import * as usuarioRepository from "../../services/usuario.repository";
+
+export default function LoginPage() {
+
+    const [email, setEmail] = useState('');
+    const [senha, setSenha] = useState('');
+    const [listUsuarios, setListUsuarios] = useState<Usuario[]>([]);
+
+    // usuarioRepository.removeAll();
+
+    useEffect(() => {
+        usuarioRepository.listUsers().then(result => setListUsuarios(result));
+    }, [listUsuarios]);
+
+    useEffect(() => {
+        AddUserDefault();
+    }, []);
+    
+    const navigation = useNavigation<ScreenNavigationProp["navigation"]>();
+
+    function validateUser(emailForm: string, senhaForm: string) { 
+        let isValid = false;
+        const result = listUsuarios.filter(user => user.email === emailForm && user.senha === senhaForm);
+        if (result.length > 0) {
+            isValid = true;
+        }
+
+        return isValid;
+    }
+
+    function AddUserDefault() { 
+        const userDefault = {
+            nome: "UserTeste",
+            email: "teste@gmail.com",
+            senha: "123"
+        } as Usuario;
+        
+        const usarioExiste = listUsuarios.filter(user => user.email === userDefault.email && user.senha === userDefault.senha);
+        if (usarioExiste.length === 0) {
+            usuarioRepository.addUser(userDefault);
+        }
+    }
+    
+    function handleSignInPress() {        
+        if (email.length === 0 || senha.length === 0) {
+            Alert.alert('Preencha email e senha para continuar!');
+        } else {
+            if (validateUser(email, senha)) {
+                navigation.navigate("Listagem", { usuario: {} as Usuario } );
+            } else {
+                Alert.alert("Houve um problema com o login, verifique suas credenciais!");
+            }
+        }
+    };
+
+    return (
+        <View style={styles.container}>
+            <Image source={require('../../images/logo.png')} style={styles.form_img} resizeMode="contain" />
+            
+            <TextInput 
+                style={styles.form_input}
+                value={email}
+                placeholder='Email' 
+                onChangeText={(email) => setEmail(email)}
+                autoCapitalize={'none'}
+                keyboardType={'email-address'}
+                autoCorrect={false}
+            />
+            <TextInput 
+                style={styles.form_input}
+                value={senha}
+                placeholder='Senha' 
+                secureTextEntry
+                onChangeText={(senha) => setSenha(senha)}
+                autoCorrect={false}
+            />
+            <TouchableOpacity onPress={() => {handleSignInPress()}}>
+                <View style={styles.button}>
+                    <Text style={styles.button_label}>Entrar</Text>
+                </View>
+            </TouchableOpacity>
+        </View>
+    );
+}
